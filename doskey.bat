5d19a8a4099690ce9fed22f4cdcf4df60ef3b712
半个果冻皮 <161580347+hengze436493@users.noreply.github.com>
2025-03-11 16:37:59 +0800

    版本更新_2025.03.11

---
+++ doskey.bat
@@ -0,0 +1,102 @@
+echo 你的wine记事本内容为：
+echo 空 %这是一个注释%
+echo --
+
+
+
+%#%
+doskey 123=start https://www.123pan.com/  %123云盘%
+
+doskey 302="D:\system-wine0\steamcommunity_302\steamcommunity_302.exe"  %302%
+ 
+%A%
+
+doskey 4399=start https://y.4399.com/detail/34  %4399云星铁%
+
+%B%
+doskey bzybj=start https://zion.functorz.com/tool/ZJ2x09KGX4P/WEB  %宝藏页编辑%
+doskey bzbj=start https://zion.functorz.com/tool/ZJ2x09KGX4P/WEB  %宝藏编辑%
+doskey bzy=start https://g3n8pwgr6l0v2.cave.functorz.com/  %宝藏页%
+doskey b=start https://space.bilibili.com/3493112452221175/favlist?fid=2005054475&ftype=create  %B站%
+doskey bdut=start https://graph.baidu.com/pcpage/index?tpl_from=pc  %百度识图%
+
+doskey qdq="D:\system-wine0\Starward_Portable_0.13.5_x64\Starward.exe" %启动器%
+doskey bh="D:\system-wine0\mhy\miHoYoLauncher\games\Star Rail Game\StarRail.exe"  %崩坏%
+
+%C%
+doskey chat=start https://www.doubao.com/chat/59976916344578  %豆包%
+doskey csdn=start https://mp.csdn.net/mp_blog/manage/article  %csdn%
+doskey iq=shutdown -r -t 0  %重启%
+
+doskey cad="D:\system-wine0\freecad\bin\freecad.exe"  %freecad%
+
+%D%
+doskey d=start https://www.douyin.com/user/self?from_tab_name=main  %抖音%
+doskey dlg=start https://www.duolingo.com/learn  %多邻国%
+
+%E%
+%F%
+%G%
+doskey gj=shutdown -s -t 0  %关机%
+doskey github=start https://github.com/  %github%
+
+%H%
+doskey huv=explorer.exe shell:RecycleBinFolder  %回收站%
+doskey hwy=start https://cloud.huawei.com/  %华为云%
+
+doskey hbyx=start https://www.petalmail.com/#/  %花瓣邮箱%
+
+%I%
+%J%
+doskey jsjgl=%windir%\system32\compmgmt.msc /s  %计算机管理%
+
+%K%
+doskey kvmb=control  %控制面板%
+
+%L%
+doskey l=start https://p7w6le0ox41d0.cave.functorz.com/  %网站%
+
+doskey llq=D:\system-wine0\Huawei\Browser\HuaweiBrowser.exe  %浏览器%
+
+%M%
+doskey mbyx=start https://cps-test.click/cn/aim-training-game  %目标游戏%
+
+%N%
+%O%
+%P%
+
+doskey plc=start D:\system-wine0\plc\FXTRN.exe  %plc%
+
+%Q%
+%R%
+%S%
+doskey shdf=start https://bzpt.shdf.gov.cn/report/frontend/?from=wx#/external/1  %扫黄打非%
+doskey system32=start C:\Windows\System32 %system32%
+doskey srdhjb=start https://wechat.12321.cn/  %骚扰电话举报%
+
+%T%
+
+doskey tim=start D:\system-wine0\tim\Bin\TIM.exe  %tim%
+
+%U%
+%V%
+%W%
+doskey wvbj=start https://zion.functorz.com/tool/X57jbwZdkDw/WEB  %网站编辑%
+doskey wjgb=start https://snapdrop.net/#  %文件广播%
+doskey wx=start https://wx.qq.com/  %微信%
+doskey wyyx=start https://email.163.com/  %网易邮箱%
+
+doskey wechat=start D:\system-wine0\wechat\Weixin\Weixin.exe  %wechat%
+doskey wecom=D:\system-wine0\wecom\WXWork\WXWork.exe  %wecom%
+
+%X%
+doskey xqivu=start https://www.starcoders.net/game  %星球创站师%
+doskey xxwv=start https://小小网站.mysxl.cn/  %小小网站%
+
+%Y%
+doskey yux=start https://rv0knp8dgqvke.cave.functorz.com/  %演示项%
+doskey yubj=start https://zion.functorz.com/tool/GQAnbZeMd5g/WEB/  %演示编辑%
+doskey yyw=start https://www.gequbao.com/  %音乐网%
+
+%Z%
+doskey vcbbjq=regedit  %注册表编辑器%
